c69ff22aecc9217b54d27164d8d131ecbf02a766
thiagoperest <thiago_teodoroperes07@hotmail.com>
2025-09-18 15:52:39 -0300

    test: Rubrica 2 - item 2 - add teste para o endpoint criar mensalista

--- src/test/java/br/edu/infnet/dr3atparte1/Dr3AtParte1ApplicationTests.java
+++ src/test/java/br/edu/infnet/dr3atparte1/Dr3AtParte1ApplicationTests.java
@@ -1,6 +1,8 @@
 package br.edu.infnet.dr3atparte1;
 
 import br.edu.infnet.dr3atparte1.config.RouteConfig;
+import br.edu.infnet.dr3atparte1.dto.MensalistaRequestDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.testtools.JavalinTest;
 import org.junit.jupiter.api.DisplayName;
@@ -25,6 +27,31 @@ class Dr3AtParte1ApplicationTests {
             assertEquals(200, response.code());
 
             assertTrue(response.body().string().contains("Hello, Javalin!"));
+        });
+    }
+
+    @Test
+    @DisplayName("Teste de integração do endpoint /mensalistas (POST)")
+    void testCreateMensalistaEndpoint() {
+        // Arrange
+        Javalin app = Javalin.create();
+        RouteConfig.configureRoutes(app);
+
+        // Act & Assert
+        JavalinTest.test(app, (server, client) -> {
+            try {
+                MensalistaRequestDto request = new MensalistaRequestDto("M004", "Thiago Peres", "Desenvolvedor de Software III", 18000.0);
+                ObjectMapper mapper = new ObjectMapper();
+                String jsonBody = mapper.writeValueAsString(request);
+
+                var response = client.post("/mensalistas", jsonBody);
+
+                assertEquals(201, response.code());
+
+                assertTrue(response.body().string().contains("Thiago Peres"));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         });
     }
 
